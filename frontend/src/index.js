4df5c65d741b59ce474959525d48f756df9aa854
keting lu <luketing1999@gmail.com>
2024-08-27 10:58:26 +0800

    feat: add vite variable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,31 @@ import { store } from './store/store'
 import { ToastProvider } from './components/Toast/ToastContext'
 import { ConfigProvider, theme } from 'antd'
 
-createRoot(document.getElementById('root')).render(
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+const apiHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST
+const apiKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY
+
+posthog.init(apiKey, {
+  api_host: apiHost,
+  person_profiles: 'identified_only',
+})
+
+const AppWrapper = () => (
   <Provider store={store}>
     <ToastProvider>
       <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
         <App />
       </ConfigProvider>
     </ToastProvider>
-  </Provider>,
+  </Provider>
+)
+createRoot(document.getElementById('root')).render(
+  apiKey && apiHost ? (
+    <PostHogProvider client={posthog}>
+      <AppWrapper />
+    </PostHogProvider>
+  ) : (
+    <AppWrapper />
+  ),
 )
