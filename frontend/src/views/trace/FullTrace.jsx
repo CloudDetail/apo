0d15e6459c2ff4f2749f88bbe731d80ea8e594e2
keting lu <luketing1999@gmail.com>
2024-09-04 14:34:40 +0800

    fix: middleware toast bug

--- frontend/src/views/trace/FullTrace.jsx
+++ frontend/src/views/trace/FullTrace.jsx
@@ -5,21 +5,6 @@ import { IoMdInformationCircleOutline } from 'react-icons/io'
 function FullTrace() {
   return (
     <div className="text-xs h-full">
-      <CToast autohide={false} visible={true} className="align-items-center w-full mb-2">
-        <div className="d-flex">
-          <CToastBody className=" flex flex-row items-center text-xs">
-            <IoMdInformationCircleOutline size={20} color="#f7c01a" className="mr-1" />
-            采集中间件指标的配置方式请参考
-            <a
-              className="underline text-sky-500"
-              target="_blank"
-              href="https://originx.kindlingx.com/docs/APO%20向导式可观测性中心/配置指南/监控中间件"
-            >
-              文档
-            </a>
-          </CToastBody>
-        </div>
-      </CToast>
       <iframe src={'/jaeger/search'} width="100%" height="100%" frameBorder={0}></iframe>
     </div>
   )
