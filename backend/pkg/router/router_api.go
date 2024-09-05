8240081fd02ca8f37762e796dd833a569b170d1b
yunguannjg <18757593931@163.com>
2024-09-05 13:35:30 +0800

    fix: update api

--- backend/pkg/router/router_api.go
+++ backend/pkg/router/router_api.go
@@ -66,6 +66,8 @@ func setApiRouter(r *resource) {
 	{
 		traceHandler := trace.New(r.logger, r.ch)
 		traceApi.POST("/pagelist", traceHandler.GetTracePageList())
+		traceApi.GET("/filters", traceHandler.GetTraceFilters())
+		traceApi.POST("/filter/value", traceHandler.GetTraceFilterValue())
 	}
 
 	alertApi := r.mux.Group("/api/alerts")
