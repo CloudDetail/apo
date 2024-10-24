19dbe421a5a5416719a120bbfc486f53362bea54
unknown <1770352033@qq.com>
2024-10-24 17:52:05 +0800

    remove vite proxy

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -47,14 +47,6 @@ export default defineConfig(() => {
     server: {
       port: 3000,
       proxy: {
-        "/api": {
-          target: "http://192.168.1.6:31363",
-          changeOrigin: true,
-          headers:{
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090c11)XWEB/11275'
-          },
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
       },
     },
   }
