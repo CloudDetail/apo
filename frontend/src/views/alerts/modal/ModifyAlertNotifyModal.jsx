3c7036e6dfb59bd1d9d9a474d7e84d81632da09d
unknown <1770352033@qq.com>
2024-10-30 15:35:50 +0800

    增加异常通知页面配置企业微信进行配置公共，修复告警通知列表显示问题和编辑问题

--- frontend/src/views/alerts/modal/ModifyAlertNotifyModal.jsx
+++ frontend/src/views/alerts/modal/ModifyAlertNotifyModal.jsx
@@ -1,5 +1,5 @@
 import { Card, Form, Input, Modal, Select, Tag, Tooltip } from 'antd'
-import _ from 'lodash'
+import _, { reject } from 'lodash'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { addAlertNotifyApi, updateAlertNotifyApi } from 'src/api/alerts'
@@ -7,7 +7,7 @@ import { showToast } from 'src/utils/toast'
 import EmailConfigsFormList from './compontents/EmailConfigsFormList'
 import WebhookConfigsFormList from './compontents/WebhookConfigsFormList'
 import DingTalkConfigsFormList from './compontents/DingTalkConfigsFormList'
-import { formatTimeStr } from 'antd/es/statistic/utils'
+import WeChatConfigsFormList from './compontents/WeChatConfigsFormList'
 export default function ModifyAlertNotifyModal({
   modalVisible,
   notifyInfo = null,
@@ -37,70 +37,106 @@ export default function ModifyAlertNotifyModal({
   };
 
   const saveRule = () => {
-    console.log(form.getFieldsValue(true))
+    console.log(form.getFieldsValue(true));
     form
       .validateFields()
       .then(() => {
-        const formState = form.getFieldsValue(true)
-
+        const formState = form.getFieldsValue(true);
         let amConfigReceiver = {
           name: formState.name,
-        }
+        };
+
         if (formState.notifyType === 'email') {
           amConfigReceiver.emailConfigs = formState.emailConfigs?.map((item) => {
             let config = Object.keys(item).reduce((acc, key) => {
               if (item[key] !== '' && !['smarthost', 'smarthostPort'].includes(key)) {
-                acc[key] = item[key]
+                acc[key] = item[key];
               }
-              return acc
-            }, {})
+              return acc;
+            }, {});
             if (item.smarthost && item.smarthostPort) {
-              config.smarthost = item.smarthost + ':' + item.smarthostPort
+              config.smarthost = item.smarthost + ':' + item.smarthostPort;
             }
             config.tlsConfig = {
               insecureSkipVerify: true,
-            }
-            config.requireTls = item.requireTls
-            return config
-          })
+            };
+            config.requireTls = item.requireTls;
+            return config;
+          }).filter(Boolean);
         } else if (formState.notifyType === 'webhook') {
           amConfigReceiver.webhookConfigs = formState.webhookConfigs?.map((item) => {
-            let config = {}
-            if (item.url) config.url = item.url
-            config.httpConfig = {}
+            let config = {};
+            if (item.url) config.url = item.url;
+            config.httpConfig = {};
             if (item.authType === 'user' && item.basicAuthUsername && item.basicAuthPassword) {
               config.httpConfig.basicAuth = {
                 username: item.basicAuthUsername,
                 password: item.basicAuthPassword,
-              }
+              };
             } else if (item.authType === 'token' && item.bearerToken) {
-              config.httpConfig.bearerToken = item.bearerToken
+              config.httpConfig.bearerToken = item.bearerToken;
             }
 
             if (item.webhookConfigsHeader?.length > 0) {
-              let headers = {}
+              let headers = {};
               item.webhookConfigsHeader?.forEach((header) => {
-                headers[header?.key] = { values: [header.value] }
-              })
-              config.httpConfig.httpHeaders = headers
+                headers[header?.key] = { values: [header.value] };
+              });
+              config.httpConfig.httpHeaders = headers;
             }
             config.httpConfig.tlsConfig = {
               insecureSkipVerify: true,
-            }
-            return config
-          })
+            };
+            return config;
+          }).filter(Boolean);
         } else if (formState.notifyType === 'dingtalk') {
           amConfigReceiver.dingTalkConfigs = formState.dingTalkConfigs?.map((item) => {
-            let config = {}
-            if (item.url) config.url = item.url
-            if (item.secret) config.secret = item.secret
-            return config
-          })
+            let config = {};
+            if (item.url) config.url = item.url;
+            if (item.secret) config.secret = item.secret;
+            return config;
+          }).filter(Boolean);
+        } else if (formState.notifyType === 'wechat') {
+          amConfigReceiver.wechatConfigs = formState.wechatConfigs?.map((item) => {
+            let flag = false;
+            let config = {};
+            if (item.api_url) config.api_url = item.api_url;
+            if (item.api_secret) config.api_secret = item.api_secret;
+            if (item.corp_id) config.corp_id = item.corp_id;
+            if (item.agent_id) config.agent_id = item.agent_id;
+            if (item.to_user) {
+              config.to_user = item.to_user;
+              flag = true;
+            }
+            if (item.to_party) {
+              config.to_party = item.to_party;
+              flag = true;
+            }
+            if (item.to_tag) {
+              config.to_tag = item.to_tag;
+              flag = true;
+            }
+            if (!flag) {
+              return null;
+            }
+            return config;
+          }).filter(Boolean);
         }
-        updateAlertNotify(amConfigReceiver, formState.notifyType)
+
+        if (
+          (formState.notifyType === 'email' && !amConfigReceiver.emailConfigs?.length) ||
+          (formState.notifyType === 'webhook' && !amConfigReceiver.webhookConfigs?.length) ||
+          (formState.notifyType === 'dingtalk' && !amConfigReceiver.dingTalkConfigs?.length) ||
+          (formState.notifyType === 'wechat' && !amConfigReceiver.wechatConfigs?.length)
+        ) {
+          return;
+        }
+
+        updateAlertNotify(amConfigReceiver, formState.notifyType);
       })
-      .catch((error) => console.log(error))
-  }
+      .catch((error) => console.log(error));
+  };
+
   useEffect(() => {
     //console.log(notifyInfo)
     if (notifyInfo && modalVisible) {
@@ -163,16 +199,56 @@ export default function ModifyAlertNotifyModal({
           // webhookConfigsHeader: webhookConfigsHeader,
         }
       })
+      const dingTalkConfigs = notifyInfo?.dingTalkConfigs?.map((config) => {
+        return {
+          url: config.url,
+          secret: config.secret
+        }
+      })
+      const wechatConfigs = notifyInfo?.wechatConfigs?.map((config) => {
+        return {
+          api_url: config.api_url,
+          api_secret: config.api_secret,
+          corp_id: config.corp_id,
+          agent_id: config.agent_id,
+          to_user: config.to_user,
+          to_party: config.to_party,
+          to_tag: config.to_tag
+        }
+      })
       form.setFieldsValue({
         name: notifyInfo.name,
-        notifyType: emailConfigs?.length > 0 ? 'email' : 'webhook',
+        notifyType: judgmentType(typeList.find(item => Object.hasOwn(notifyInfo, item))),
         emailConfigs,
         webhookConfigs,
+        dingTalkConfigs,
+        wechatConfigs
       })
     } else {
       form.resetFields()
     }
   }, [modalVisible, notifyInfo])
+
+  const judgmentType = (type) => {
+    switch (type) {
+      case 'emailConfigs':
+        return 'email'
+      case 'webhookConfigs':
+        return 'webhook'
+      case 'dingTalkConfigs':
+        return 'dingtalk'
+      case 'wechatConfigs':
+        return 'wechat'
+    }
+  }
+
+  const typeList = [
+    'emailConfigs',
+    'webhookConfigs',
+    'dingTalkConfigs',
+    'wechatConfigs'
+  ];
+
   return (
     <>
       <Modal
@@ -221,7 +297,8 @@ export default function ModifyAlertNotifyModal({
                 options={[
                   { label: '邮件通知', value: 'email' },
                   { label: 'Webhook通知', value: 'webhook' },
-                  { label: '钉钉', value: 'dingtalk' }
+                  { label: '钉钉', value: 'dingtalk' },
+                  { label: '企业微信', value: 'wechat' }
                 ]}
                 disabled={notifyInfo}
               />
@@ -238,6 +315,7 @@ export default function ModifyAlertNotifyModal({
                   {notifyType === 'email' && <EmailConfigsFormList />}
                   {notifyType === 'webhook' && <WebhookConfigsFormList />}
                   {notifyType === 'dingtalk' && <DingTalkConfigsFormList />}
+                  {notifyType === 'wechat' && <WeChatConfigsFormList />}
                 </>
               )
             }}
