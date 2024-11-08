4d3e28c6d219c92b85f8a52475a8da3b61adb961
keting lu <luketing1999@gmail.com>
2024-11-08 17:36:43 +0800

    fix：全量日志content解析支持嵌套多层

--- frontend/src/views/logs/component/FullLogs/component/LogQueryResult/QueryList/LogItem/component/LogKeyTag.jsx
+++ frontend/src/views/logs/component/FullLogs/component/LogQueryResult/QueryList/LogItem/component/LogKeyTag.jsx
@@ -1,21 +1,30 @@
 import { Tag } from 'antd'
 import React from 'react'
 import LogTagDropDown from './LogTagDropdown'
-//Key 作为log内容
+
 const LogKeyTag = (props) => {
   const { title, description } = props
+
+  // 判断 title 和 description 是否为对象或数组，若是则转换为字符串
+  const formatValue = (value) => (typeof value === 'object' ? JSON.stringify(value) : value)
+
   return (
     <div className="flex">
       <div>
-        <Tag className="cursor-pointer  text-gray-200">{title}</Tag>
+        <Tag className="cursor-pointer text-gray-200">{formatValue(title)}</Tag>
       </div>
       ：
       <LogTagDropDown
-        objKey={title}
-        value={description}
-        children={<span className="break-all hover:underline cursor-pointer">{description}</span>}
-      ></LogTagDropDown>
+        objKey={formatValue(title)}
+        value={formatValue(description)}
+        children={
+          <span className="break-all hover:underline cursor-pointer">
+            {formatValue(description)}
+          </span>
+        }
+      />
     </div>
   )
 }
+
 export default LogKeyTag
