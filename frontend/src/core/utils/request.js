e6450bdb090e56a645dbacac7cbe6fb7a5cb4182
nuchengliu <1770352033@qq.com>
2024-11-13 12:09:21 +0800

    chore: 修改接口超时时间为120s

--- frontend/src/core/utils/request.js
+++ frontend/src/core/utils/request.js
@@ -5,7 +5,7 @@ import qs from 'qs'
 // 创建axios实例
 const instance = axios.create({
   baseURL: '', // 替换为你的API基础URL
-  timeout: 30000,
+  timeout: 120000,
   headers: { 'Content-Type': 'application/json' },
   paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
 })
