b30c106e3afb78af3c73cc3c3a7c0e5b04f73d9c
nuchengliu <1770352033@qq.com>
2024-12-11 13:37:03 +0800

    chore: remove needless console code

--- frontend/src/oss/views/logs/component/FullLogs/component/LogQueryResult/QueryList/LogItem/component/LogTagDropdown.jsx
+++ frontend/src/oss/views/logs/component/FullLogs/component/LogQueryResult/QueryList/LogItem/component/LogTagDropdown.jsx
@@ -6,7 +6,6 @@ import { useLogsContext } from 'src/core/contexts/LogsContext'
 const LogTagDropDown = ({ objKey, value, children, trigger = ['click', 'contextMenu'] }) => {
   const { query, updateQuery } = useLogsContext()
   const addToQuery = () => {
-    console.log("valueType", typeof value)
     let newQueryPart
     switch (typeof value) {
       case 'string':
@@ -22,7 +21,6 @@ const LogTagDropDown = ({ objKey, value, children, trigger = ['click', 'contextM
         newQueryPart = '`' + objKey + '` = -' + "'" + value + "'"
         break
     }
-    console.log(newQueryPart)
     // 检查 query 是否已经包含 newQueryPart
     if (!query.includes(newQueryPart)) {
       let newQuery = query
