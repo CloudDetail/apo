d5e940351f29f5d77f62528296f1dc0de6870cfd
Mislu <ymislu@163.com>
2024-12-19 10:59:06 +0800

    chore: update sql scripts

--- backend/sqlscripts/default_role_permission.sql
+++ backend/sqlscripts/default_role_permission.sql
@@ -90,3 +90,11 @@ UNION ALL
 SELECT 4, 'role', 'feature', 7 WHERE NOT EXISTS (SELECT 1 FROM auth_permission WHERE subject_id = 4 AND subject_type = 'role' AND type = 'feature' AND permission_id = 7)
 UNION ALL
 SELECT 4, 'role', 'feature', 8 WHERE NOT EXISTS (SELECT 1 FROM auth_permission WHERE subject_id = 4 AND subject_type = 'role' AND type = 'feature' AND permission_id = 8)
+UNION ALL
+SELECT 4, 'role', 'feature', 9 WHERE NOT EXISTS (SELECT 1 FROM auth_permission WHERE subject_id = 4 AND subject_type = 'role' AND type = 'feature' AND permission_id = 9)
+UNION ALL
+SELECT 4, 'role', 'feature', 10 WHERE NOT EXISTS (SELECT 1 FROM auth_permission WHERE subject_id = 4 AND subject_type = 'role' AND type = 'feature' AND permission_id = 10)
+UNION ALL
+SELECT 4, 'role', 'feature', 11 WHERE NOT EXISTS (SELECT 1 FROM auth_permission WHERE subject_id = 4 AND subject_type = 'role' AND type = 'feature' AND permission_id = 11)
+UNION ALL
+SELECT 4, 'role', 'feature', 13 WHERE NOT EXISTS (SELECT 1 FROM auth_permission WHERE subject_id = 4 AND subject_type = 'role' AND type = 'feature' AND permission_id = 13)
