0aa42e7627461ce6c1ec8c1fa7d97ad45da945e2
LIN <111079387+Qingmy@users.noreply.github.com>
2024-12-20 13:46:02 +0800

    fix: abnormal filtering of full log fields (#131)
    * fix: abnormal filtering of full log fields
    
    * chore: Modify render logic
    
    * chore: Adjust code structure

--- frontend/src/oss/views/logs/component/FullLogs/component/LogQueryResult/QueryList/LogItem/component/LogItemFold.jsx
+++ frontend/src/oss/views/logs/component/FullLogs/component/LogQueryResult/QueryList/LogItem/component/LogItemFold.jsx
@@ -3,14 +3,15 @@ import LogValueTag from './LogValueTag';
 import LogKeyTag from './LogKeyTag';
 import { useMemo } from 'react';
 
-const LogItemFold = ({ tags, fields }) => {
+const LogItemFold = ({ tags }) => {
   const { tableInfo, displayFields } = useLogsContext();
-
+  //由tableName和type组成的唯一标识
+  const tableId = `${tableInfo.tableName}_${tableInfo.type}`
   // 计算过滤后的 tags
   const filteredTags = useMemo(() => {
     return Object.entries(tags).filter(([key, value]) => {
       return (
-        displayFields.includes(key) &&  // 检查是否在显示字段中
+        displayFields[tableId]?.includes(key) &&  // 检查是否在显示字段中
         value !== '' &&  // 确保值不为空
         key !== (tableInfo?.timeField || 'timestamp') &&  // 排除时间字段
         typeof value !== 'object'  // 确保值不是对象
@@ -18,26 +19,12 @@ const LogItemFold = ({ tags, fields }) => {
     });
   }, [tags, displayFields, tableInfo?.timeField]);
 
-  // 计算过滤后的 fields
-  const filteredFields = useMemo(() => {
-    return fields ? Object.entries(fields).filter(([key, value]) => displayFields.includes(key)) : [];
-  }, [fields, displayFields]);
-
   return (
     <>
       {/* 渲染 tags */}
       <div className="text-ellipsis text-wrap flex" style={{ display: '-webkit-box' }}>
         {filteredTags.map(([key, value]) => (
           <LogValueTag key={key} objKey={key} value={String(value)} />
-        ))}
-      </div>
-
-      {/* 渲染 fields */}
-      <div className="text-ellipsis text-wrap flex flex-col overflow-hidden">
-        {filteredFields.map(([key, value]) => (
-          <div key={key}>
-            <LogKeyTag title={key} description={value} />
-          </div>
         ))}
       </div>
     </>
--- frontend/src/oss/views/logs/component/FullLogs/component/LogQueryResult/QueryList/LogItem/index.jsx
+++ frontend/src/oss/views/logs/component/FullLogs/component/LogQueryResult/QueryList/LogItem/index.jsx
@@ -6,31 +6,16 @@ import { Button } from 'antd'
 import { useLogsContext } from 'src/core/contexts/LogsContext'
 import { convertTime } from 'src/core/utils/time'
 
-// 自配类规则日志默认展开不可收起，tag+铺平（仅content）
-// 接入类数据库规则默认收起可展开，收起展示所有tag，展开展示所有（content + tag）
 const LogItem = (props) => {
-  const { log, foldingChecked, openContextModal } = props
+  const { log, openContextModal } = props
   const { tableInfo } = useLogsContext()
-  // 是否折叠日志，true 为是，false 为否
-  const [isFold, setIsFold] = useState(true)
 
-  useEffect(() => {
-    setIsFold(foldingChecked ?? true)
-  }, [foldingChecked])
   return (
     <div className="flex overflow-hidden px-2">
       {/* icon 和 时间 */}
       <div className="flex-grow-0 flex-shrink-0  w-[230px]">
         <div className="items-center pl-2 j">
           <div className="flex-shrink-0 flex-grow-0 flex items-center">
-            {tableInfo.timeField && (
-              <Button
-                color="primary"
-                type="text"
-                onClick={() => setIsFold(!isFold)}
-                icon={isFold ? <AiFillCaretRight /> : <AiFillCaretDown />}
-              ></Button>
-            )}
             <span>{convertTime(log?.timestamp, 'yyyy-mm-dd hh:mm:ss.SSS')}</span>
           </div>
           {openContextModal && !tableInfo.timeField && (
@@ -48,25 +33,12 @@ const LogItem = (props) => {
       </div>
       {/* 具体日志 */}
       <div className="flex-1 overflow-hidden">
-        {/* <LogItemFold log={log} isFold={isFold} />
-        <LogItemDetail log={log} isFold={isFold} /> */}
-        {/* <LogItemFold tags={!tableInfo?.timeField || isFold ? log.tags : []} /> */}
-        {tableInfo.timeField ? (
+        {
           <>
-            {
-              isFold ? (
-                <LogItemFold tags={log.tags} fields={log.logFields} />
-              ) : (
-                <LogItemDetail log={log} />
-              )
-            }
+            <LogItemFold tags={log.tags} />
+            <LogItemDetail log={log} contentVisibility={!tableInfo?.timeField} />
           </>
-        ) : (
-          <>
-            <LogItemFold tags={log.tags} fields={log.logFields} />
-            <LogItemDetail log={log} />
-          </>
-        )}
+        }
       </div>
     </div>
   )
