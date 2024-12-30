4bf9cb607bfa063cde0f7d649dc1975f99c0b8f5
nuchengliu <1770352033@qq.com>
2024-12-30 14:29:20 +0800

    chore：Supplementary license header

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2024 CloudDetail
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
