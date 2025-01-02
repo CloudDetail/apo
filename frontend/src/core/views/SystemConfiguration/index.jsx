162f66c1ebc4166b8c293f94ee6372e540aaa18a
nuchengliu <1770352033@qq.com>
2025-01-02 10:49:18 +0800

    chore: Supplementary copyright information

--- frontend/src/core/views/SystemConfiguration/index.jsx
+++ frontend/src/core/views/SystemConfiguration/index.jsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2024 CloudDetail
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React from 'react'
 import { CCard } from '@coreui/react'
 import LanguageSwitcher from 'src/core/components/LanguageSwitcher'
