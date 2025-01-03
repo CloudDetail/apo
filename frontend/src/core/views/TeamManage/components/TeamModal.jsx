da860f87ceaa44dc4af0e81cd8e0170183b35f94
nuchengliu <1770352033@qq.com>
2025-01-03 11:24:39 +0800

    remove unexpect file TeamModal

--- frontend/src/core/views/TeamManage/components/TeamModal.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Modal } from 'antd'
-
-const TeamModal = (props) => {
-  const { open, closeModal } = props
-  return <Modal open={open} onCancel={closeModal}></Modal>
-}
-
-export default TeamModal
