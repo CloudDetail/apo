a24979e43248e1b5ba1c022f09692de2b60b9aaf
MisluNotFound <108245592+MisluNotFound@users.noreply.github.com>
2025-01-14 10:53:28 +0800

    chore: remove menu icon. (#175)

--- backend/pkg/repository/database/dao_menu.go
+++ backend/pkg/repository/database/dao_menu.go
@@ -8,7 +8,7 @@ type MenuItem struct {
 	ItemID       int    `gorm:"column:item_id;primary_key" json:"itemId"`
 	Key          string `gorm:"column:key;uniqueIndex" json:"key"`
 	Label        string `gorm:"-" json:"label"` // AKA item name.
-	Icon         string `gorm:"column:icon" json:"icon"`
+	Icon         string `gorm:"column:icon" json:"icon,omitempty"`
 	ParentID     *int   `gorm:"column:parent_id" json:"-"`
 	Abbreviation string `gorm:"-" json:"abbreviation,omitempty"`
 	RouterID     int    `gorm:"column:router_id" json:"-"`
