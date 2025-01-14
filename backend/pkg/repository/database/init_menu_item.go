4fee4a097201295d2a41550e3464dc33dac08b31
MisluNotFound <108245592+MisluNotFound@users.noreply.github.com>
2025-01-14 10:26:00 +0800

    feat: Add system configuration menu. (#174)
    * feat: Add system configuration menu.
    
    * feat: Add system configuration menu.

--- backend/pkg/repository/database/init_menu_item.go
+++ backend/pkg/repository/database/init_menu_item.go
@@ -31,6 +31,7 @@ func (repo *daoRepo) initMenuItems() error {
 		{MenuItem: MenuItem{Key: "manage", Icon: "https://apo-front.oss-cn-hangzhou.aliyuncs.com/menu-icon/system.svg", Order: 16}},
 		{MenuItem: MenuItem{Key: "userManage", Order: 17}, RouterKey: "/system/user-manage"},
 		{MenuItem: MenuItem{Key: "menuManage", Order: 18}, RouterKey: "/system/menu-manage"},
+		{MenuItem: MenuItem{Key: "systemConfig", Order: 19}, RouterKey: "/system/config"},
 	}
 
 	return repo.db.Transaction(func(tx *gorm.DB) error {
@@ -69,6 +70,7 @@ func (repo *daoRepo) initMenuItems() error {
 			"menuManage":     "manage",
 			"alertsRule":     "alerts",
 			"alertsNotify":   "alerts",
+			"systemConfig":   "manage",
 		}
 
 		// update parent_id
