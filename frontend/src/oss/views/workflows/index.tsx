c9e51c764dd3de4cd64652f0da135e88fdff6d63
keting lu <luketing1999@gmail.com>
2025-03-09 11:40:41 +0800

    fix: workflows page iframe src

--- frontend/src/oss/views/workflows/index.tsx
+++ frontend/src/oss/views/workflows/index.tsx
@@ -3,7 +3,7 @@ import WorkflowsIframe from './workflowsIframe'
 const WorkflowsPage = () => {
   return (
     <div className="text-xs" style={{ height: 'calc(100vh - 120px)' }}>
-      <WorkflowsIframe src="/apps" />
+      <WorkflowsIframe src="/dify/apps" />
     </div>
   )
 }
