698d7382a0a9c2db514c1d40f06fa3ecca175eb2
yunguannjg <18757593931@163.com>
2025-03-11 17:13:24 +0800

    fix: avoid nil pointer error

--- backend/pkg/core/context.go
+++ backend/pkg/core/context.go
@@ -230,6 +230,9 @@ func (c *context) ClientIP() string {
 }
 
 func (c *context) LANG() string {
+	if c == nil {
+		return code.LANG_ZH
+	}
 	lang := c.GetHeader("APO-Language")
 	if strings.HasPrefix(strings.ToLower(lang), code.LANG_EN) {
 		return code.LANG_EN
