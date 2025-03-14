3e057ba51a97e7a5f0be02a93f5e4e2c8d66d6e3
yunguannjg <18757593931@163.com>
2025-03-14 11:13:46 +0800

    refactor: move alert error types to errors.go and delete redundant error.go file

--- backend/pkg/model/integration/alert/errors.go
+++ backend/pkg/model/integration/alert/errors.go
@@ -77,3 +77,38 @@ func (e ErrAlertImpactNoMatchedService) CheckedTagGroup() string {
 
 	return fmt.Sprintf("[%s]", strings.Join(checkedGroup, ", "))
 }
+
+type ErrAlertSourceAlreadyExist struct {
+	Name string
+}
+
+func (e ErrAlertSourceAlreadyExist) Error() string {
+	return fmt.Sprintf("alertsource %s is already existed", e.Name)
+}
+
+type ErrAlertSourceNotExist struct{}
+
+func (e ErrAlertSourceNotExist) Error() string {
+	return "alertSource is not existed"
+}
+
+type ErrNotAllowSchema struct {
+	Table  string
+	Column string
+}
+
+func (e ErrNotAllowSchema) Error() string {
+	if len(e.Table) > 0 {
+		return fmt.Sprintf("not allowed table: %s", e.Table)
+	}
+
+	return fmt.Sprintf("not allowed column: %s", e.Column)
+}
+
+type ErrIllegalAlertRule struct {
+	Err error
+}
+
+func (e ErrIllegalAlertRule) Error() string {
+	return fmt.Sprintf("illegal alert rule: %v", e.Err)
+}
