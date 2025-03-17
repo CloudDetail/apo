00752bf44dd580c13375dd36b4ae70f67bcb1b9e
keting lu <luketing1999@gmail.com>
2025-03-17 17:38:36 +0800

    feat: anonymous login dify

--- frontend/src/core/api/workflows.ts
+++ frontend/src/core/api/workflows.ts
@@ -3,9 +3,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { post } from "../utils/request"
+import { get, post } from "../utils/request"
 
 
 export const workflowLoginApi = (params) =>{
-    return post('/dify/console/api/login',params)
+    return post('/dify/console/api/login/apo',params)
+}
+export const workflowLogoutApi = () =>{
+    return get('/dify/console/api/logout')
+}
+export const workflowAnonymousLoginApi = (params) =>{
+    return post('/dify/console/api/login/apo/anonymous',params)
 }
