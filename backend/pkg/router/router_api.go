8b38c1be91580bf933901b65713ef2a19e33e2a0
ymislu <ymislu@163.com>
2025-03-27 16:44:23 +0800

    chore: Remove auth from trace list api.

--- backend/pkg/router/router_api.go
+++ backend/pkg/router/router_api.go
@@ -100,11 +100,13 @@ func setApiRouter(r *resource) {
 	traceApi := r.mux.Group("/api/trace")
 	{
 		traceHandler := trace.New(r.logger, r.pkg_db, r.ch, r.jaegerRepo, r.prom, r.k8sApi)
+		// These APIs are used by another project. DO NOT Auth.
 		traceApi.GET("/onoffcpu", traceHandler.GetOnOffCPU())
 		traceApi.GET("/flame", traceHandler.GetFlameGraphData())
 		traceApi.GET("/flame/process", traceHandler.GetProcessFlameGraph())
-		traceApi.Use(middlewares.AuthMiddleware())
 		traceApi.POST("/pagelist", traceHandler.GetTracePageList())
+		
+		traceApi.Use(middlewares.AuthMiddleware())
 		traceApi.GET("/pagelist/filters", traceHandler.GetTraceFilters())
 		traceApi.POST("/pagelist/filter/value", traceHandler.GetTraceFilterValue())
 		traceApi.GET("/info", traceHandler.GetSingleTraceInfo())
@@ -116,6 +118,7 @@ func setApiRouter(r *resource) {
 		alertApi.POST("/event/list", alertHandler.AlertEventList())
 		alertApi.POST("/inputs/alertmanager", alertHandler.InputAlertManager())
 		alertApi.POST("/outputs/dingtalk/:uuid", alertHandler.ForwardToDingTalk())
+
 		alertApi.Use(middlewares.AuthMiddleware())
 		alertApi.GET("/rules/file", alertHandler.GetAlertRuleFile())
 		alertApi.POST("/rules/file", alertHandler.UpdateAlertRuleFile())
@@ -215,6 +218,7 @@ func setApiRouter(r *resource) {
 	k8sApi := r.mux.Group("/api/k8s")
 	{
 		k8sHandler := k8s.New(r.k8sApi)
+		// These APIs are used by another project. DO NOT Auth.
 		k8sApi.GET("/namespaces", k8sHandler.GetNamespaceList())
 		k8sApi.GET("/namespace/info", k8sHandler.GetNamespaceInfo())
 		k8sApi.GET("/pods", k8sHandler.GetPodList())
