a795fc56372e601f0acabf3e08924c0bfb40403e
PoorShawn <18352979520@163.com>
2025-03-31 06:09:45 -0300

    chore: add license header in CustomCard

--- frontend/src/core/components/Card/CustomCard.tsx
+++ frontend/src/core/components/Card/CustomCard.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 CloudDetail
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { Card } from "antd"
 import type { CardProps } from "antd/es/card";
 import { ReactNode, CSSProperties } from "react";
