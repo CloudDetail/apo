1d9cec7037e99687787ab86f586410511a8222ac
PoorShawn <18352979520@163.com>
2025-04-14 09:28:35 +0300

    chore: update translations in AuthRouter

--- frontend/src/core/components/AuthRouter.tsx
+++ frontend/src/core/components/AuthRouter.tsx
@@ -1,8 +1,14 @@
+/**
+ * Copyright 2025 CloudDetail
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Spin } from 'antd'
 import FallbackPage from 'src/core/components/FallbackPage'
 import { getRouterPermissionApi } from '../api/permission'
+import { useTranslation } from 'react-i18next'
 
 const checkRoutePermission = async (route: string) => {
   const authResult = await getRouterPermissionApi({ router: route });
@@ -13,6 +19,7 @@ const AuthRouter = (WrappedComponent) => {
   return function GuardedComponent(props) {
     const [isAllowed, setIsAllowed] = useState(null)
     const location = useLocation()
+    const { t } = useTranslation('common')
 
     useEffect(() => {
       const checkPermission = async () => {
@@ -30,7 +37,7 @@ const AuthRouter = (WrappedComponent) => {
       )
     }
 
-    return isAllowed ? <WrappedComponent {...props} /> : <FallbackPage errorInfo='当前没有权限访问该路由' />
+    return isAllowed ? <WrappedComponent {...props} /> : <FallbackPage errorInfo={t('routeError')} />
   }
 }
 
