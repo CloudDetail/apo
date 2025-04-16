cae82bd681074f6e90f6aec2ba99271e2e2a18d9
yunguannjg <18757593931@163.com>
2025-04-16 17:47:33 +0800

    fix: use endTime to check resolved alert

--- backend/pkg/services/alerts/service_alert_event_list.go
+++ backend/pkg/services/alerts/service_alert_event_list.go
@@ -46,8 +46,14 @@ func (s *service) AlertEventList(req *request.AlertEventSearchRequest) (*respons
 }
 
 func (s *service) fillWorkflowParams(record *alert.AEventWithWRecord) {
-	startTime := record.UpdateTime.Add(-15 * time.Minute)
-	endTime := record.UpdateTime
+	var startTime, endTime time.Time
+	if record.AlertEvent.Status == alert.StatusResolved {
+		startTime = record.EndTime.Add(-15 * time.Minute)
+		endTime = record.EndTime
+	} else {
+		startTime = record.UpdateTime.Add(-15 * time.Minute)
+		endTime = record.UpdateTime
+	}
 
 	record.WorkflowParams = alert.WorkflowParams{
 		StartTime: startTime.UnixMicro(),
