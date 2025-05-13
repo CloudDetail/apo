80b5d9e85885a4c679cc66b2e907f294ce53b5b9
keting lu <luketing1999@gmail.com>
2025-05-13 16:35:20 +0800

    chore: Custom Tags theme

--- frontend/src/oss/views/alertEvents/components/AlertInfoCom.tsx
+++ frontend/src/oss/views/alertEvents/components/AlertInfoCom.tsx
@@ -6,7 +6,6 @@ import { Button, Tag, Tooltip } from 'antd'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import ReactJson from 'react-json-view'
-import CostomTag from 'core/components/Tag/Tag'
 import { t } from 'i18next'
 function isJSONString(str: string) {
   try {
@@ -86,9 +85,7 @@ const AlertStatus = ({
   if (!status) return
   return (
     <div className="text-center">
-      <CostomTag type={status === 'firing' ? 'error' : 'success'}>
-        {t(`oss/alertEvents:${status}`)}
-      </CostomTag>
+      <Tag color={status === 'firing' ? 'error' : 'success'}>{t(`oss/alertEvents:${status}`)}</Tag>
       {status === 'resolved' && resolvedTime && (
         <span className="text-[10px] block text-gray-400">
           {t('oss/alertEvents:resolvedOn')} {resolvedTime}
