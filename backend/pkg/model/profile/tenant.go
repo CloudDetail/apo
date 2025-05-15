124b424f40a2dc5d7e73fa331f603c35a5cb25f0
yunguannjg <18757593931@163.com>
2025-05-15 20:56:33 +0800

    refactor: remove unused struct

--- backend/pkg/model/profile/tenant.go
+++
@@ -1,9 +0,0 @@
-package profile
-
-type Tenant struct {
-	TenantID int `gorm:"column:tenant_id;primary_key" json:"tenantId"`
-
-	Name string `gorm:"column:name" json:"name"`
-	// TODO More detail fields
-	Info string `gorm:"column:info" json:"info"`
-}
