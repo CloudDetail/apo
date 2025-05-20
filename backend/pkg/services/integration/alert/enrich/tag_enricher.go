7beb5fecba2c6aa493b3e863944d14a3afb4dba4
yunguannjg <18757593931@163.com>
2025-05-20 14:46:37 +0800

    fix: use regexp stringSubmatch

--- backend/pkg/services/integration/alert/enrich/tag_enricher.go
+++ backend/pkg/services/integration/alert/enrich/tag_enricher.go
@@ -112,8 +112,15 @@ func (e *TagEnricher) Enrich(alertEvent *alert.AlertEvent) {
 	}
 
 	if e.fromPattern != nil {
-		value = e.fromPattern.FindString(value)
-	}
+		vs := e.fromPattern.FindStringSubmatch(value)
+		if len(vs) > 1 {
+			value = vs[1] // Extract first capture group
+		} else if len(vs) == 1 {
+			value = vs[0] // Extract entire match (no capture group)
+		} else {
+			value = "" // No match found
+		}
+	} // else: retain original value if e.fromPattern is nil
 
 	switch e.RType {
 	case "tagMapping":
