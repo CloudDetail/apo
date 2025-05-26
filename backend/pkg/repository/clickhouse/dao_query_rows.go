700b577b60899e8f629812a0bfdea43ea356958d
yunguannjg <18757593931@163.com>
2025-05-26 16:02:28 +0800

    fix: adjust clickhouse row accept struct

--- backend/pkg/repository/clickhouse/dao_query_rows.go
+++ backend/pkg/repository/clickhouse/dao_query_rows.go
@@ -3,7 +3,12 @@
 
 package clickhouse
 
-import core "github.com/CloudDetail/apo/backend/pkg/core"
+import (
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	core "github.com/CloudDetail/apo/backend/pkg/core"
+)
 
 func (ch *chRepo) queryRowsData(ctx core.Context, sql string) ([]map[string]any, error) {
 	rows, err := ch.GetContextDB(ctx).Query(ctx.GetContext(), sql)
@@ -11,25 +16,71 @@ func (ch *chRepo) queryRowsData(ctx core.Context, sql string) ([]map[string]any,
 		return nil, err
 	}
 	defer rows.Close()
+	return rowsToMapSlice(rows)
+}
 
-	columns := rows.Columns()
-	results := make([]map[string]interface{}, 0)
-	for rows.Next() {
-		values := make([]interface{}, len(columns))
-		valuePtrs := make([]interface{}, len(columns))
-		for i := range values {
-			valuePtrs[i] = &values[i]
+func rowsToMapSlice(rows driver.Rows) ([]map[string]any, error) {
+	columnNames := rows.Columns()
+	columnTypes := rows.ColumnTypes()
+	var result []map[string]any
+
+	valuePtrs := make([]any, len(columnNames))
+	for i, colType := range columnTypes {
+		switch colType.DatabaseTypeName() {
+		case "DateTime64", "DateTime", "DateTime64(9)":
+			valuePtrs[i] = new(time.Time)
+		case "UInt64":
+			valuePtrs[i] = new(uint64)
+		case "Int64":
+			valuePtrs[i] = new(int64)
+		case "UInt32":
+			valuePtrs[i] = new(uint32)
+		case "Int32":
+			valuePtrs[i] = new(int32)
+		case "Float64":
+			valuePtrs[i] = new(float64)
+		case "Float32":
+			valuePtrs[i] = new(float32)
+		case "Nullable(String)", "String", "FixedString", "LowCardinality(String)":
+			valuePtrs[i] = new(string)
+		case "UUID":
+			valuePtrs[i] = new(string)
+		default:
+			valuePtrs[i] = new(string)
 		}
+	}
+
+	for rows.Next() {
 		if err := rows.Scan(valuePtrs...); err != nil {
 			return nil, err
 		}
 
-		entry := make(map[string]interface{})
-
-		for i, col := range columns {
-			entry[col] = values[i]
+		rowMap := make(map[string]any, len(columnNames))
+		for i, name := range columnNames {
+			switch val := valuePtrs[i].(type) {
+			case *time.Time:
+				rowMap[name] = *val
+			case *uint64:
+				rowMap[name] = *val
+			case *int64:
+				rowMap[name] = *val
+			case *uint32:
+				rowMap[name] = *val
+			case *int32:
+				rowMap[name] = *val
+			case *float64:
+				rowMap[name] = *val
+			case *float32:
+				rowMap[name] = *val
+			case *string:
+				rowMap[name] = *val
+			case *any:
+				rowMap[name] = *val
+			default:
+				rowMap[name] = ""
+			}
 		}
-		results = append(results, entry)
+		result = append(result, rowMap)
 	}
-	return results, nil
+	return result, nil
 }
