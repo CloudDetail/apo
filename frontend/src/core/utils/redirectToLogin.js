bffdc6f04338204454373164e59f88da89b6ea9b
PoorShawn <18352979520@163.com>
2025-06-03 10:57:18 +0800

    chore: add license header

--- frontend/src/core/utils/redirectToLogin.js
+++ frontend/src/core/utils/redirectToLogin.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 CloudDetail
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export const redirectToLogin = (shouldRecordUrl = true) => {
   if (shouldRecordUrl) {
     const currentUrl = window.location.href;
