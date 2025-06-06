a3d7d175a34cd7869367825d488cfc4e520d94dd
PoorShawn <18352979520@163.com>
2025-06-06 18:02:45 +0800

    chore: add license header

--- frontend/src/core/components/Drawer/PreferencesDrawer.tsx
+++ frontend/src/core/components/Drawer/PreferencesDrawer.tsx
@@ -1,9 +1,12 @@
+/**
+ * Copyright 2025 CloudDetail
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { Button, Drawer, Segmented } from "antd"
 import { IoLanguageOutline } from "react-icons/io5"
 import { VscColorMode } from "react-icons/vsc"
-import { LogoutOutlined, UserOutlined, SunFilled, MoonFilled, SunOutlined, MoonOutlined, CloseOutlined, SettingOutlined } from '@ant-design/icons'
-import { MdTune } from "react-icons/md";
-import { useEffect, useState } from "react";
+import { SunFilled, MoonFilled, CloseOutlined, SettingOutlined } from '@ant-design/icons'
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { useColorModes } from "@coreui/react";
@@ -46,7 +49,7 @@ const PreferencesDrawer = ({ open, onClose }) => {
       title={
         <div className="flex justify-start items-center gap-2">
           <SettingOutlined />
-          {t('preferences')}
+          <p className="p-1">{t('preferences')}</p>
         </div>
       }
       placement='right'
