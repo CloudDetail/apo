80e59d282953c81a2003fa698309bf74a70604c0
yunguannjg <18757593931@163.com>
2025-06-16 18:19:23 +0800

    fix: add record

--- backend/pkg/repository/clickhouse/dao_alert_event_detail.go
+++ backend/pkg/repository/clickhouse/dao_alert_event_detail.go
@@ -60,9 +60,9 @@ SELECT ae.id as id,
   fw.importance as importance,
   fw.output as output,
   fw.created_at as last_check_at,
-  fw.alert_direction,
-  fw.analyze_run_id,
-  fw.analyze_err,
+  fw.alert_direction as alert_direction,
+  fw.analyze_run_id as analyze_run_id,
+  fw.analyze_err as analyze_err,
   le.status as last_status,
   CASE
     WHEN output = 'false' THEN 'true'
@@ -120,9 +120,9 @@ SELECT ae.id as id,
   fw.created_at as last_check_at,
   fw.importance as importance,
   fw.output as output,
-  fw.alert_direction,
-  fw.analyze_run_id,
-  fw.analyze_err,
+  fw.alert_direction as alert_direction,
+  fw.analyze_run_id as analyze_run_id,
+  fw.analyze_err as analyze_err,
   CASE
     WHEN output = 'false' THEN 'true'
     WHEN output = 'true' THEN 'false'
