e950d8300a848ada528c65cefcafe7f3662728a1
PoorShawn <18352979520@163.com>
2025-06-20 09:30:11 +0800

    chore: remove duplicate components

--- frontend/src/core/components/AppHeader.js
+++ frontend/src/core/components/AppHeader.js
@@ -7,7 +7,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import logo from 'src/core/assets/brand/logo.svg'
-import { SkinOutlined } from '@ant-design/icons';
+import { SkinOutlined } from '@ant-design/icons'
 import { CHeader, CHeaderNav, useColorModes, CImage } from '@coreui/react'
 import { SettingOutlined } from '@ant-design/icons'
 import { AppBreadcrumb } from './index'
@@ -32,7 +32,7 @@ const AppHeader = ({ type = 'default' }) => {
   const dispatch = useDispatch()
   const sidebarShow = useSelector((state) => state.sidebarShow)
   const [selectedKeys, setSelectedKeys] = useState([])
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false)
 
   const onClick = (to) => {
     navigate(to)
@@ -113,9 +113,13 @@ const AppHeader = ({ type = 'default' }) => {
         <CHeaderNav className="pr-4 flex items-center">
           {location.pathname === '/service/info' && <CoachMask />}
           {checkRoute() && <DateTimeCombine />}
-          <Button type="text" icon={<SkinOutlined />} onClick={() => setDrawerOpen(true)} title={t('preferences')} />
+          <Button
+            type="text"
+            icon={<SkinOutlined />}
+            onClick={() => setDrawerOpen(true)}
+            title={t('preferences')}
+          />
           <UserToolBox />
-          <PreferencesDrawer open={drawerOpen} onClose={() => setDrawerOpen(false)} />
           <PreferencesDrawer open={drawerOpen} onClose={() => setDrawerOpen(false)} />
         </CHeaderNav>
       </div>
