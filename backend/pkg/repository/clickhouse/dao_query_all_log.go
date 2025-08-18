38f577fb3e8cc21bf4bc3ae74eb33c45ea36bf9c
yunguannjg <18757593931@163.com>
2025-08-18 10:59:53 +0800

    fix: allow using different database

--- backend/pkg/repository/clickhouse/dao_query_all_log.go
+++ backend/pkg/repository/clickhouse/dao_query_all_log.go
@@ -32,6 +32,10 @@ func (ch *chRepo) QueryAllLogs(ctx core.Context, req *request.LogQueryRequest) (
 }
 
 func (ch *chRepo) QueryAllLogsInOrder(ctx core.Context, req *request.LogQueryRequest) ([]map[string]any, string, error) {
+	if len(req.DataBase) == 0 {
+		req.DataBase = ch.database
+	}
+
 	condition := NewQueryCondition(req.StartTime, req.EndTime, req.TimeField, req.Query)
 	bySql := NewByLimitBuilder().
 		OrderBy(fmt.Sprintf("`%s`", req.TimeField), true).
